e6f7445c96cfeb6119013fbd81776bfc486de847
Alfonso Ascaso <alfonso.ascaso.lizarrondo@gmail.com>
2024-04-25 17:03:20 +0200

    Mis primeros comentarios

--- src/javadoca/aritmetica.java
+++ src/javadoca/aritmetica.java
@@ -8,19 +8,50 @@ package javadoca;
  */
 public class aritmetica {
 
+    /**
+     * Suma dos números enteros.
+     *
+     * @param a El primer número entero a sumar.
+     * @param b El segundo número entero a sumar.
+     * @return La suma de los dos números enteros.
+     */
 
     public int sumar(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Resta dos números enteros.
+     *
+     * @param a El número entero del cual restar.
+     * @param b El número entero a restar.
+     * @return La diferencia entre los dos números enteros.
+     */
 
     public int restar(int a, int b) {
         return a - b;
     }
 
+    /**
+     * Multiplica dos números enteros.
+     *
+     * @param a El primer número entero a multiplicar.
+     * @param b El segundo número entero a multiplicar.
+     * @return El producto de los dos números enteros.
+     */
+
     public int multiplicar(int a, int b) {
         return a * b;
     }
+
+    /**
+     * Divide dos números enteros.
+     *
+     * @param a El dividendo.
+     * @param b El divisor.
+     * @return El cociente de la división como un double.
+     * @throws IllegalArgumentException si el divisor es cero.
+     */
 
     public double dividir(int a, int b) {
         if (b == 0) {
--- src/javadoca/operaciones.java
+++ src/javadoca/operaciones.java
@@ -8,6 +8,15 @@ package javadoca;
  */
 public class operaciones {
 
+    /**
+     * Calcula el módulo de la división entre dos números enteros.
+     *
+     * @param a El dividendo.
+     * @param b El divisor.
+     * @return El resto de la división entre a y b.
+     * @throws IllegalArgumentException si el divisor es cero.
+     */
+
     public int modulo(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("El divisor no puede ser cero");
@@ -15,10 +24,24 @@ public class operaciones {
         return a % b;
     }
 
- 
+    /**
+     * Calcula el cuadrado de un número entero.
+     *
+     * @param a El número entero para calcular su cuadrado.
+     * @return El cuadrado del número entero a.
+     */
+
     public int calcularCuadrado(int a) {
         return a * a;
     }
+
+    /**
+     * Calcula la raíz cuadrada de un número de punto flotante.
+     *
+     * @param a El número double del cual calcular la raíz cuadrada.
+     * @return La raíz cuadrada del número double a.
+     * @throws IllegalArgumentException si a es un número negativo.
+     */
 
     public double calcularRaizCuadrada(double a) {
         if (a < 0) {
